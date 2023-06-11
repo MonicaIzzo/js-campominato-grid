a5bfdc49e3c397929996fb9b8cf2b254130c3ff0
MonicaIzzo <info@monicaizzo.net>
2023-06-12 01:38:10 +0200

    add BONUS "level"

--- js/script.js
+++ js/script.js
@@ -15,24 +15,58 @@ In ogni cella, deve comparire il numero corrispondente, in ordine da 1 a 100;
 Al click sulla cella, stampiamo il numero della cella cliccata in console, poi coloriamo la cella d'azzurro!
 */
 
-// #0 Struttura HTML: Mi prepraro la sruttura del HTML + CSS.
-// #1 Svuotamento del HTML Elimino dalla pagina gli elementi che saranno "generati" tramite JS.
-// #2 Recupero dal DOM gli elementi dalla pagina tramite "getElement" con [id, class o tag].
-// #3  Setto delle variabili di comodo per gli elementi che dovrò manipolare e creo i SQUARE.
-// #3bis  Posso crearmi o utilizzare anche delle funzioni?
-// #4 applico al Bottone [Crea] il compito di generare tramite un ciclo [credo un "for"] le 100 caselle.
-// #5 Creo tramite un "event" lo script che ascolta il [click] del bottone. All' event applico la classe [background-color] al bottone e stampo in console la variabile [i] (contatore che mi ha generato le celle).
-// FINE
-// #6bis Tramite il CSS si potrebbe cambire al width e la height [penso tramite l'assegnazione di una classe diversa in JS] e una [select] - [difficoltà] che cambi le variabili da passare al ciclo [for] per generare i quadrati. [ipotesi]
-// #7bis un tasto [aggiorna] potrebbe far ripartie il ciclo [crea celle] oppure un altro modo e settare sulle [class] la classe [d-none] o soluzione ancora più semplice "svuotare il DIV" che contiene le celle sovrascivendo il contenuto con [''] [ipotesi]
-
 
 console.log ('JS OK');
 
-// FUNZIONI (#3bis)
+// FUNZIONI
+
+function startGame () {
+// cambio in ricomincia
+    playGame.innerText =
+     'Ricomincia';
+    grid.innerHTML = '';
+    
+    const level =levelSelect.value;
+
+    let rows;
+    let cols;
+
+    switch(level) {
+        case 'hard' :
+        rows = 7;
+        cols = 7;
+        break;
+        case 'normal' :
+        rows = 9;
+        cols = 9;
+        break;
+        case 'easy' :
+        default:
+        rows = 10;
+        cols = 10; 
+    }
+
+    let totalCells = rows * cols;
+
+    for (let i = 0; i < totalCells; i++) {
+    const square = createSquare();
+    square.innerText = i + 1;
+    square.classList.add(level);
+    console.log(level);
+
+    square.addEventListener('click', function () {
+        square.classList.add('hover');
+        
+        console.log(++i);
+        
+    });
+    grid.appendChild(square);
+    };
+}
+
 const createSquare = () => {
- const square = document.createElement('div');
- square.className = 'square';
+const square = document.createElement('div');
+square.classList.add('square');
 
  return square;
 }
@@ -46,47 +80,36 @@ const createSquare = () => {
 // #2 Recupero dal DOM gli elementi dalla pagina
 const grid = document.getElementById('grid');
 const playGame = document.getElementById('playGame');
-
+const levelSelect = document.getElementById('livello');
+console.log(levelSelect)
 
 // #3 Setto delle variabili di comodo e mi creo le SQUARE
 
-const rows = 10;
-const cols = 10;
-const totalCells = rows * cols;
+let rows;
+let cols;
+
+let level;
+
+if (level = 'easy') {
+    rows = 10;
+    cols = 10;
+} else if (level = 'normal') {
+    rows = 9;
+    cols = 9;
+} else {
+    rows = 7;
+    cols = 7;
+}
+
+totalCells = rows * cols;
+console.log(totalCells);
+
+
+
+
+
+
+
 
 // #4 applico al Bottone [Crea] il compito di generare i square.
-
-   playGame.addEventListener('click', function (){
-    // cambio in ricomincia
-    playGame.innerText = 'Ricomincia';
-    grid.innerText = '';
-
-    for (let i = 0; i < totalCells; i++) {
-    const square = createSquare();
-    square.innerText = i + 1;
-
-    square.addEventListener('click', function () {
-        square.classList.add('hover');
-        console.log(++i);
-    });
-    grid.appendChild(square);
-    };
-
-});
-
-
-
-    
-
-
-
-    
-
-
-
-
-
-
-
-
-// #5 Ascolto il Bottone [Crea]
+   playGame.addEventListener('click', startGame);
